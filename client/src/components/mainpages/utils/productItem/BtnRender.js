3c867a13e254b211f8e4982982d0c5e0f1547711
WeifengZhao1 <kweifeng16@gmail.com>
2022-01-26 13:55:15 -0500

    Related Product Btn_Render

--- client/src/components/mainpages/utils/productItem/BtnRender.js
+++ client/src/components/mainpages/utils/productItem/BtnRender.js
@@ -5,7 +5,7 @@ function BtnRender({product}) {
   return (
     <div className='row_btn'>
         <Link id = "btn_buy" to = "#!"> Buy</Link>
-        <Link id="btn_view" to={`detail/${product._id}`}>View</Link>
+        <Link id="btn_view" to={`/detail/${product._id}`}>View</Link>
     </div>
   )
 }
